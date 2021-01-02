a471250b47d39b61b639c2935c308ada7875a9f2
etsec-cloud <etienne.secondini@hotmail.fr>
2021-01-02 15:46:25 +0100

    pull

--- resources/js/components/HomeComponent.vue
+++ resources/js/components/HomeComponent.vue
@@ -1,13 +1,79 @@
 <template>
     <div class="container">
-        <h1>Bienvenue</h1>
+        <p class="text-center mt-5">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Tortor
+            accumsan arcu in urna, tempus tristique. Egestas sed neque a sed
+            nulla in. Accumsan, hendrerit leo, a tristique at eleifend. A, dui
+            nibh et nisl. Enim arcu nisl convallis arcu volutpat, elementum et
+            nisi, est. Urna, venenatis, aliquam viverra cras tempus, nisi vel
+            auctor velit. Nisi, molestie nec est a. Donec neque id quis eget
+            purus natoque amet iaculis sapien. Risus nunc, molestie maecenas
+            aliquam sed quam. Proin libero nibh dictumst cras. Non malesuada sed
+            odio porttitor. Diam neque nascetur tempor suspendisse nibh massa
+            leo.
+        </p>
+        <div class="d-flex justify-content-around">
+            <div class="box mt-5 p-3 border-radius1">
+                <h2>Créer une équipe</h2>
+                <p class="paragraphe">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et
+                    diam dolor diam nunc. Egestas varius convallis diam vitae
+                    morbi nibh. Consequat accumsan aliquam sed mattis nulla.
+                    Purus nisl libero, sit scelerisque. Urna, risus, aliquet
+                    rhoncus at donec. Eget purus amet sit facilisis. Maecenas eu
+                    cursus commodo diam vel, mi quam. Gravida at nunc sed risus
+                    eget vitae, id. Lobortis rhoncus aenean.
+                </p>
+                <div class="box p-1 border-radius3">Créer une équipe</div>
+            </div>
+            <div class="box mt-5 p-3 border-radius2">
+                <h2>Créer un match</h2>
+                <p class="paragraphe">
+                    Lorem ipsum dolor sit amet, consectetur adipiscing elit. Et
+                    diam dolor diam nunc. Egestas varius convallis diam vitae
+                    morbi nibh. Consequat accumsan aliquam sed mattis nulla.
+                    Purus nisl libero, sit scelerisque. Urna, risus, aliquet
+                    rhoncus at donec. Eget purus amet sit facilisis. Maecenas eu
+                    cursus commodo diam vel, mi quam. Gravida at nunc sed risus
+                    eget vitae, id. Lobortis rhoncus aenean.
+                </p>
+                <div class="box p-1 border-radius4">Créer un match</div>
+            </div>
+        </div>
     </div>
 </template>
 
 <script>
-    export default {
-        mounted() {
-            console.log('Component mounted.')
-        }
+export default {
+    mounted() {
+        console.log("Component mounted.");
     }
+};
 </script>
+<style scoped>
+.box {
+    border: 3px solid #ff7a00;
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+}
+.border-radius1 {
+    border-radius: 20px 0 0 20px;
+}
+
+.border-radius2 {
+    border-radius: 0 20px 20px 0;
+}
+
+.border-radius3 {
+    border-radius: 10px 0 0 10px;
+}
+
+.border-radius4 {
+    border-radius: 0 10px 10px 0;
+}
+
+.paragraphe {
+    max-width: 400px;
+}
+</style>
